bf627c6e029837bfb28baf1c9a4d8b6fc5246eab
albertsparrman <albert.sparrman@gmail.com>
2024-05-13 00:44:48 +0200

    nav pointers

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -10,23 +10,37 @@ import { CommonModule } from '@angular/common';
 })
 export class NavigationComponent {
   smallMenu: boolean = false
-  home: boolean = false
+  home: boolean = true
   projects: boolean = false
   about: boolean = false
   contact: boolean = false
+  hero: number | undefined = 0
 
   @HostListener('window:scroll', ['$event'])
 
   onScroll() {
+    this.hero = document.getElementById("hero")?.offsetHeight
     if (window.scrollY < 1) {
       this.smallMenu = false
     }
     else {
       this.smallMenu = true
     }
-
-    if(window.scrollY > 1 && window.screenY < 200 ) {
-      this.home = true
+    if (this.hero != undefined) {
+      if (window.scrollY < this.hero - 100) {
+        this.home = true
+      }
+      else {
+        this.home = false
+      }
+    }
+    if (this.hero != undefined) {
+      if (window.scrollY > this.hero - 100) {
+        this.projects = true
+      }
+      else {
+        this.projects = false
+      }
     }
   }
 
