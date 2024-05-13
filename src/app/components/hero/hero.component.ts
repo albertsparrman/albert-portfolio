f8fd8b4ea586465fa24b2dd944d2f55bd502ad08
albertsparrman <albert.sparrman@gmail.com>
2024-05-13 03:40:55 +0200

    Hero Carousel

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -9,5 +9,53 @@ import { CommonModule } from '@angular/common';
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  currentArticle: number = 1
 
+  heroArticle1: boolean = true
+  heroArticle2: boolean = false
+  heroArticle3: boolean = false
+
+  resetHeroBooleans() {
+    this.heroArticle1 = false
+    this.heroArticle2 = false
+    this.heroArticle3 = false
+  }
+
+  setHeroBooleansValue() {
+    if(this.currentArticle == 1){
+      this.heroArticle1 = true
+    }
+    else if (this.currentArticle == 2) {
+      this.heroArticle2 = true
+    }
+    else if (this.currentArticle == 3) {
+      this.heroArticle3 = true
+    }
+  }
+
+  heroScrollLeft() {
+    this.resetHeroBooleans()
+
+    if (this.currentArticle > 1) {
+      this.currentArticle -= 1
+    }
+    else {
+      this.currentArticle = 3
+    }
+
+    this.setHeroBooleansValue()
+  }
+
+  heroScrollRight() {
+    this.resetHeroBooleans()
+
+    if (this.currentArticle < 3) {
+      this.currentArticle += 1
+    }
+    else {
+      this.currentArticle = 1
+    }
+    
+    this.setHeroBooleansValue()
+  }
 }
