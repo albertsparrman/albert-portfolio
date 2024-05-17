706e6799bc581e51a5b62ec604c1b4200dd0d830
Albert <albert.sparrman@gmail.com>
2024-05-17 15:50:35 +0200

    json

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import projects  from '../../../assets/projects.json'
 
 @Component({
   selector: 'app-projects',
@@ -9,6 +10,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projectsJson: {name: string, description: string}[] = projects
+
+  test() {
+    console.log(this.projectsJson[0].name)
+  }
+  
   scrollToElement($element: string): void {
     document.getElementById($element)?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
   }
